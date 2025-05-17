6dbacc21e9283a4d7929f2d0ccb8ed701e460884
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 15:42:42 +0300

    feat: Add db initialization with creating users table

--- model.py
+++ model.py
@@ -1,2 +1,16 @@
 import sqlite3
 from datetime import datetime
+
+def init_db():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id              INTEGER PRIMARY KEY AUTOINCREMENT,
+            telegram_id     INTEGER UNIQUE NOT NULL,
+            username        TEXT,
+            is_authorized   BOOLEAN DEFAULT 0
+        );
+    ''')
+    connection.commit()
+    connection.close()
