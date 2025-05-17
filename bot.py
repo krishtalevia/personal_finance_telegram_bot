26f1ac40f1a550c36f81fd05c0505604628ea868
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 21:19:16 +0300

    chore: Include transactions handlers

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ async def main():
     dp.include_routers(auth.router)
     dp.include_routers(income.router)
     dp.include_routers(expense.router)
+    dp.include_routers(transactions.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
