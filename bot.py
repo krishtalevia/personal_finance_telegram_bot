e07a72e949fb22a6683a88358e8fd0a48d4dfdfd
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 21:31:31 +0300

    chore: Include goals from handlers

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ async def main():
     dp.include_routers(income.router)
     dp.include_routers(expense.router)
     dp.include_routers(transactions.router)
+    dp.include_routers(goals.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
