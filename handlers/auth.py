241bd9e38d92d91a40057da7534ec92ee3f56dd0
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 19:21:11 +0300

    feat(handlers): Register handler feature

--- handlers/auth.py
+++ handlers/auth.py
@@ -5,3 +5,19 @@ from model import DatabaseManager
 
 router = Router()
 db_manager = DatabaseManager()
+
+@router.message(StateFilter(None), Command('register'))
+async def register_handler(message: types.Message):    
+    telegram_id = message.from_user.id
+    try:
+        if db_manager.register_user(telegram_id):
+            await message.answer('✅ Вы успешно зарегистрировались. Для авторизации используйте /login')
+    
+    except ValueError as ve:
+        if str(ve) == 'Пользователь уже существует!':
+            await message.answer('⚠️ Вы уже зарегистрированы. Используйте /login для авторизации')
+        else:
+            await message.answer(f'❌ Ошибка: {ve}')
+    
+    except Exception as e:
+        await message.answer('❌ Во время регистрации произошла непредвиденная ошибка.')
