d265978b4a91b04beb2e2263b9ce72d7b5108a69
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 21:07:06 +0300

    chore(handlers): Add placeholder for view transaction handler

--- handlers/transactions.py
+++ handlers/transactions.py
@@ -31,3 +31,7 @@ def get_date_range_for_period(period_keyword):
     if period_start:
         return period_start.strftime('%Y-%m-%d'), period_end.strftime('%Y-%m-%d')
     return None, None
+
+@router.message(Command('view_transactions'))
+async def view_transactions_handler(message: types.Message):
+    pass
