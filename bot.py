1fc9081db0f012f9d334e95b363d816040f6718b
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 15:21:42 +0300

    feat: Add main

--- bot.py
+++ bot.py
@@ -5,3 +5,18 @@ from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import auth, start
 from model import init_db
+
+async def main():
+    bot = Bot(token=TOKEN) 
+    dp = Dispatcher()
+
+    init_db()
+
+    dp.include_routers(start.router)
+    dp.include_routers(auth.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
