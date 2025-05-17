5f41e3039408e3f0b70d4a0da270682dc1f97617
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-17 20:42:49 +0300

    chore(handlers): Add states (FSM)

--- handlers/expense.py
+++ handlers/expense.py
@@ -7,3 +7,8 @@ from model import DatabaseManager
 
 router = Router()
 db_manager = DatabaseManager()
+
+class AddExpenseStates(StatesGroup):
+    AddingAmount = State()
+    AddingCategory = State()
+    Confirmation = State()
