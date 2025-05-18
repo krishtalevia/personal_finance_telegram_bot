301553b53904a759218f8c63d739a7cbe6cacafd
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-18 14:04:58 +0300

    feat(tests): Use standart db init

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,3 +7,12 @@ def db_manager():
     db_file_to_use = 'database.db'
     if os.path.exists(db_file_to_use):
         os.remove(db_file_to_use)
+
+    init_db()
+    manager = DatabaseManager(db_name=db_file_to_use)
+
+    yield manager
+    
+    manager.close()
+    if os.path.exists(db_file_to_use):
+        os.remove(db_file_to_use)
