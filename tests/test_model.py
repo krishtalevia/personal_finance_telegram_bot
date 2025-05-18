a88a6e1d31897bfe3e7f660b9a1150db2f440b26
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-18 14:05:41 +0300

    feat(tests): Add test 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -16,3 +16,12 @@ def db_manager():
     manager.close()
     if os.path.exists(db_file_to_use):
         os.remove(db_file_to_use)
+
+# Тестовый случай 1.1: Попытка регистрации с уже существующим в системе идентификатором пользователя.
+def test_register_existing_user(db_manager):
+    telegram_id = 12345
+    
+    db_manager.register_user(telegram_id)
+
+    with pytest.raises(ValueError, match='Пользователь уже существует!'):
+        db_manager.register_user(telegram_id)
