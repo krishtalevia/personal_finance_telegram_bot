671e7056bde76706c90df6e148a35fea0d0258cd
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-18 14:09:34 +0300

    feat(tests): Add test 1.3

--- tests/test_model.py
+++ tests/test_model.py
@@ -37,3 +37,13 @@ def test_register_new_user(db_manager):
     assert user_data is not None
     assert user_data[1] == telegram_id
     assert user_data[2] == 0
+
+# Тестовый случай 1.3: Авторизация с правильными учетными данными (зарегистрированного пользователя).
+def test_authorize_registered_user(db_manager):
+    telegram_id = 22222
+    db_manager.register_user(telegram_id)
+
+    result = db_manager.authorize_user(telegram_id)
+    assert result is True
+
+    assert db_manager.is_user_authorized(telegram_id) is True
