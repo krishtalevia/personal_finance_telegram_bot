81993db5054b3b6201e85ca3cac4367529811751
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-18 12:33:53 +0300

    chore(handlers): Help handler placeholder

--- handlers/help.py
+++ handlers/help.py
@@ -2,3 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import Command
 
 router = Router()
+
+@router.message(Command('help'))
+async def help_handler(message: types.Message):
+    pass
