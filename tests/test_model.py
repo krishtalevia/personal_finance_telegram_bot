9b89a07d5cd9504749f1c5ac292e06cd82c712c4
krishtalevia <krishtalev.ilya@gmail.com>
2025-05-18 14:01:37 +0300

    feat(tests): Test db with selfdelete

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,8 @@
 import pytest
 import os
 from model import DatabaseManager, init_db
+
+@pytest.fixture
+def db_manager():
+    if os.path.exists('test_finance_bot.db'):
+        os.remove('test_finance_bot.db')
